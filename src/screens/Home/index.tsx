d0024020629560b0fd230b9179e34ebdf4069a4b
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-07-01 15:34:32 -0300

    inputs e botoes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
-export  function Home() {
+export function Home() {
+  const handleParticipantAdd = () => {
+    console.log("Você clicou no botão de adicionar participante");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6b6b6b"}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,4 +15,32 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: "#6b6b6b",
   },
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#fdfcfe",
+    padding: 16,
+    fontSize: 16,
+    flex: 1,
+    marginRight: 12,
+  },
+  buttonText: {
+    color: "#fdfcfe",
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31cf67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  }
 });
